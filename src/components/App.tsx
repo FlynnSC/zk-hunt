a395f6c32d50121287f7c717803caef439afc2ee
Flynn Calcutt <flynn.calcutt@hotmail.com>
2022-09-28 12:34:06 +0100

    Removed bad import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,6 @@ import {publicProvider} from 'wagmi/providers/public';
 import {BrowserRouter} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
 import {styling} from './styling';
 
 const {chains, provider} = configureChains(
